b949405e5c3d2ee6d03943b73e35e60e09a3aeba
Adhi Satria Banyuaji <53932334+Banyuaji@users.noreply.github.com>
2020-06-16 19:02:37 +0700

    error upload

--- upload.php
+++ upload.php
@@ -1,4 +1,23 @@
 <?php
     if (isset($_POST['uploadBtn']) && $_POST['uploadBtn'] == 'Upload') {
-        # code...
+        $fileTmpPath = $_FILES['uploadedFile']['tmp_name'];
+        $fileName = $_FILES['uploadedFile']['name'];
+        $fileSize = $_FILES['uploadedFile']['size'];
+        $fileType = $_FILES['uploadedFile']['type'];
+        $fileNameCmps =  explode(".", $fileName);
+        $fileExtension = strtolower(end($fileNameCmps));
+
+        $newFileName = md5(time() . $fileName) . '.' . $fileExtension;
+
+        $allowedFileExtensions = array('jpg','gif','zip','txt','xls','doc');
+        if (in_array($fileExtension, $allowedFileExtensions)) {
+            $uploadFileDir = 'upload_files/';
+            $dest_path = $uploadFileDir . $newFileName;
+
+            if (move_uploaded_file($fileTmpPath, $dest_path)) {
+                $message = 'File is successfully uploaded.';
+            } else {
+                $message = 'There was some error moving the file to upload directory. Please make sure the upload directory is writable by web browser';
+            }
+        }
     }
